e87c0c88a612f6d48eae4635e136a8a4ebcc4109
lutiku <55091206+lutiku@users.noreply.github.com>
2019-12-18 19:17:46 +0100

    Add files via upload

---
+++ src/animalerie.kt
@@ -0,0 +1,72 @@
+import java.util.*
+
+/*Interface Animale*/
+
+
+interface Animal
+    {
+
+        var parler : String
+        var quisuisje : String
+
+       fun parlerMethod() : String
+           {
+               return(parler)
+           }
+       fun quisuisjeMethod() :String
+            {
+                return(quisuisje)
+            }
+
+    }
+
+
+
+/*Les classes d'animaux qu'il faut*/
+
+class chat
+{
+ var parler:String = "miaou"
+ var quisuisje:String= "je suis un chat"
+}
+
+class chien
+{
+    var parler:String="wouf"
+    var quisuisje:String = "je suis un chien"
+}
+
+class peroquet
+{
+    var parler:String="repeat"
+    var quisuisje:String="je suis un peroquet"
+}
+
+
+/*Fonction main*/
+
+fun animal()
+    {
+        var scan = Scanner(System.`in`)
+        println("Ajouter un animal :")
+
+        var parler: String = scan.next()
+        var quisuisje: String = scan.next()
+
+        println("un animal a été ajouté !")
+        print(parler)
+        print(quisuisje)
+
+
+
+    }
+
+/*Class Animalerie
+
+class Animalerie : Animal
+    {
+
+    }
+
+*/
+
